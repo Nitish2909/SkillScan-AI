e05271fb2ee97e649839ccddebf397d1ef14c965
Nitish2909 <nitishraj7043@gmail.com>
2026-03-08 16:21:10 +0530

    Backend updated

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+     origin: "https://skillscan-ai-website.onrender.com",
     credentials: true,
   }),
 );
